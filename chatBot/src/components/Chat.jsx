ba42ee9b33d6318c1f310003eac8401e2988bed2
Ranjan00001 <ranjansingh3357@gmail.com>
2024-09-18 10:31:44 +0530

    Beautified styling, added message component to work like pro

--- chatBot/src/components/Chat.jsx
+++ chatBot/src/components/Chat.jsx
@@ -1,23 +1,41 @@
 import '../css/Chat.css'
 import SideBar from './SideBar'
 import Message from './Message'
-import { useState } from 'react'
+import MessageBox from './MessageBox'
+import { useState, useRef, useEffect } from 'react'
+
 function Chat() {
     const [selectedBot, setSelectedBot] = useState(null)
+    const [newMessage, setNewMessage] = useState([])
+    const reference = useRef()
+
+    useEffect(() => {
+        reference.current.scrollIntoView({ behavior: "smooth" });
+    }, [newMessage]);
 
     function handleClick(botName) {
         setSelectedBot(botName)
+    }
+    function handleSend(message) {
+        setNewMessage((newMessage) => [...newMessage, message])
+        message = ''
     }
 
     return (
         <>
             <div className='chat'>
-                <SideBar handleClick={handleClick}/>
-                <div className='message'>
-                    <Message botName={selectedBot}/>
+                <SideBar handleClick={handleClick} />
+                <div>
+                    <div className='messageContainer'>
+                        {newMessage.map((msg, index) => (
+                            <Message key={index} botName={selectedBot} newMessage={msg} />
+
+                        ))}
+                        <div ref={reference}></div>
+                    </div>
+                    <MessageBox handleSend={handleSend} />
                 </div>
             </div>
-
         </>
     )
 }
--- chatBot/src/components/Message.jsx
+++ chatBot/src/components/Message.jsx
@@ -2,33 +2,26 @@ import { useState } from 'react'
 import '../css/Message.css'
 import MessageBox from './MessageBox'
 import user from '../assets/user.png'
+import bot from '../assets/bot.png'
 
-function Message({ botName }) {
-    const [newMessage, setNewMessage] = useState(null)
-
-    function handleSend(message) {
-        setNewMessage(message)
-        message = ''
-    }
+function Message({ botName, newMessage }) {
 
     return (
         <>
-            <div className='header'>
-                <h1>Prompt {botName}</h1>
+            {newMessage &&
 
-            </div>
-            <div className='messageContainer'>
-                <div className='query'>
-                    <div>{newMessage}</div>
-                    <img src={user} alt='userImage'></img>
-                </div>
-                <div className='reply'>
-                    {/* {newMessage && reply(newMessage, operation)} */}
-                </div>
-            </div>
-            <div>
-                <MessageBox handleSend={handleSend} />
-            </div>
+                <div className='prevMessage'>
+                    <div className='query'>
+                        <div className='queryBox' >{newMessage}</div>
+                        <img src={user} alt='userImage'></img>
+                    </div>
+                    <div className='reply'>
+                        <img src={bot} alt='botImage'></img>
+                        {/* {Response(newMessage)} */}
+                        <div className='replyBox' >{newMessage}</div>
+                    </div>
+                </div>}
+            
         </>
     )
 }
